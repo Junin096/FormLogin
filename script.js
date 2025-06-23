da9d6bd57a23a9e987b178644989d9d17bdac535
Junin096 <ivan.goncalves.junior@escola.pr.gov.br>
2025-06-23 08:48:14 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById("loginform").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errormessage = document.getElementById("error-mensage");
 
+//Simula credenciais validas (Não usar assim em produção)
+const validUsername = "admin";
+const validPassword = "123456";
+
+if (username === validUsername && password === validPassword) {
+    alert("Login bem sucedido");
+    //Aqui voce pode redirecionar para outra pagina
+    window.location.href = "www.google.com" ;
+} else {
+  errormessage.textContent  = "Usuario ou senha incorreto"
+}
 });
